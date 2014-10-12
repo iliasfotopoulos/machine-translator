1f8494f3a3f3e20a22e26dd01b4ad2c25171fc23
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-10-12 12:25:47 +0300

    English language rules

--- machine_translator.pl
+++ machine_translator.pl
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Greek rules
+% Greek Language rules
 
 % Seperate rules for 11,12
 
@@ -23,6 +23,41 @@ elnum(X,[[Y,Z],X]) :-
 
 % Rule for  X < 1000                                
 elnum(X,[[N,Y,Z],X]) :-
+                                integer(X), X < 1000,
+                                N is floor(X / 100) * 100, % Ν = hundreds
+                                Y is X mod 100-X mod 10, % Y = tens
+                                Z is X mod 10. % Z = units
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% English Language rules
+
+ennum(11,[[11],11]):- !.
+ennum(12,[[12],12]):- !.
+ennum(13,[[13],13]):- !.
+ennum(14,[[14],14]):- !.
+ennum(15,[[15],15]):- !.
+ennum(16,[[16],16]):- !.
+ennum(17,[[17],17]):- !.
+ennum(18,[[18],18]):- !.
+ennum(19,[[19],19]):- !.
+
+% Rule for 1-9
+ennum(X,[[X],X]):-
+                        integer(X),X < 10,!.
+
+% Rule for 10,20 ... 90
+ennum(X,[[X],X]):-
+                        integer(X),X < 100,
+                        X mod 10 =:= 0,!.
+
+% Rule for  X < 100
+ennum(X,[[Y,Z],X]) :-
+                                integer(X), X < 100,
+                                Y is floor(X / 10) * 10, % Υ = tens
+                                Z is X mod 10. % Z = units
+
+% Rule for  X < 1000                                
+ennum(X,[[N,Y,Z],X]) :-
                                 integer(X), X < 1000,
                                 N is floor(X / 100) * 100, % Ν = hundreds
                                 Y is X mod 100-X mod 10, % Y = tens
