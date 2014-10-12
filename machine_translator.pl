ee08a78ce7543e030e773ec76fc302fe9ccd4a9e
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-10-12 13:13:59 +0300

    Add greek dictionary

--- machine_translator.pl
+++ machine_translator.pl
@@ -71,9 +71,9 @@ el(N) --> ekatontades(N1),dekades(N2),monades(N3),
 {N is N1 + N2 + N3,N =\= 11, N =\= 12}.
 el(N) --> dekades(N1),monades(N2),
 {N is N1 + N2,N =\= 11, N =\= 12}.
-el(N)-->ekatontades(N1),dekades(N2),
+el(N)--> ekatontades(N1),dekades(N2),
         {N is N1 + N2,N =\= 11, N =\= 12}.
-el(N)-->ekatontades(N1),monades(N2),
+el(N)--> ekatontades(N1),monades(N2),
         {N is N1 + N2,N =\= 11, N =\= 12}.
 
 
@@ -88,3 +88,37 @@ dekades(N) --> deka(N);eikosi(N);trianta(N);saranta(N);penhnta(N);
 
 monades(N) --> ena(N);duo(N);tria(N);tessera(N);pente(N);
                 exi(N);efta(N);oktw(N);ennia(N).
+
+mhden(0) --> [μηδέν].
+ena(1) --> [ένα].
+duo(2) --> [δύο].
+tria(3) --> [τρία].
+tessera(4) --> [τέσσερα].
+pente(5) --> [πέντε].
+exi(6) --> [έξι].
+efta(7) --> [εφτά].
+oktw(8) --> [οχτώ].
+ennia(9) --> [εννιά].
+deka(10) --> [δέκα].
+enteka(11) --> [έντεκα].
+dwdeka(12) --> [δώδεκα].
+eikosi(20) --> [είκοσι].
+trianta(30) --> [τριάντα].
+saranta(40) --> [σαράντα].
+penhnta(50) --> [πενήντα].
+exhnta(60) --> [εξήντα].
+ebdomhnta(70) --> [εβδομήντα].
+ogdonta(80) --> [ογδόντα].
+ennenhnta(90) --> [ενενήντα].
+ekato(100)--> [εκατό].
+ekaton(100) --> [εκατόν].
+diakosia(200) --> [διακόσια].
+triakosia(300) --> [τριακόσια].
+tetrakosia(400) --> [τετρακόσια].
+pentakosia(500) --> [πεντακόσια].
+exakosia(600) --> [εξακόσια].
+eftakosia(700) --> [εφτακόσια].
+oktakosia(800) --> [οχτακόσια].
+enniakosia(900) --> [εννιακόσια].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
